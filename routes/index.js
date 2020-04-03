34a8141eb215811f78aaff6060d7603e7b115f12
Kiran Joseph <kiran.joseph.maliekal@gmail.com>
2020-04-03 13:05:27 -0400

    added Products

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page. localhost:port/ */
 router.get('/', function(req, res, next) {
   console.log("hello")
   res.render('index', { title: 'Express' });
