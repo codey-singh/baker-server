a4e42d15befa6dd5c38a4202a88db7f570f4778b
bhajanpreet <bhajanpreet25@gmail.com>
2020-04-03 20:14:07 -0400

    added more functionality

--- README.md
+++ README.md
@@ -16,3 +16,15 @@ npm run debug   //or
 npm start
 
 ```
+
+### Database Structure
+
+1. Inserting data in products collection
+
+```
+db.products.insertMany([
+  {name:"Whiteforest Cake", desc: "Three layers of vanilla sponge cake with vanilla flavored whip cream and fresh berry filling in between.", price: 20, image:"whiteforest.jpeg" },
+  {name:"Candy Cake", desc: "Layers of chocolate sponge cake with decadent chocolate sauce and assorted candy toppings.", price: 20, image:"candycake.jpeg" },
+  {name:"Vanilla Buttercream Cake", desc: "Unicorn theme birthday cake with layers of butter cake and vanilla buttercream frosting.", price: 20, image:"vanilla.jpeg" }
+])
+```
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,19 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-
+var { connect } = require("../database/connection");
 /* GET home page. localhost:port/ */
-router.get('/', function(req, res, next) {
-  console.log("hello")
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  connect((client, db) => {
+    let collection = db.collection("products");
+    collection
+      .find({})
+      .limit(3)
+      .toArray()
+      .then((products) => {
+        res.render("index", { title: "Express", products });
+        client.close();
+      });
+  });
 });
 
 module.exports = router;
