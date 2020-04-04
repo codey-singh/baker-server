425ce3cf3e82480a2346f06c69db1b55c26a1bb5
bhajanpreet <bhajanpreet25@gmail.com>
2020-04-03 21:22:14 -0400

    Added filestore and session capability

--- app.js
+++ app.js
@@ -1,51 +1,69 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const uuid = require("uuid/v4");
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var productsRouter = require("./routes/products");
+var aboutusRouter = require("./routes/aboutus");
+var faqsRouter = require("./routes/faqs");
+var contactusRouter = require("./routes/contactus");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var productsRouter = require('./routes/products');
-var aboutusRouter = require('./routes/aboutus');
-var faqsRouter = require('./routes/faqs');
-var contactusRouter = require('./routes/contactus');
-var registerRouter = require('./routes/register');
-var loginRouter = require('./routes/login');
+const session = require("express-session");
+const FileStore = require("session-file-store")(session);
+
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/products', productsRouter);
-app.use('/about', aboutusRouter);
-app.use('/faqs', faqsRouter);
-app.use('/contactus', contactusRouter);
-app.use('/register', registerRouter);
-app.use('/login', loginRouter);
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(
+  session({
+    genid: (req) => {
+      console.log("Inside session middleware genid function");
+      console.log(`Request object sessionID from client: ${req.sessionID}`);
+      return uuid(); // use UUIDs for session IDs
+    },
+    store: new FileStore(),
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 3600000, // 1 hour
+    },
+  })
+);
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/products", productsRouter);
+app.use("/about", aboutusRouter);
+app.use("/faqs", faqsRouter);
+app.use("/contactus", contactusRouter);
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
