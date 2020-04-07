ea24fb8e6672689ab7c18cd9e838d38ec8e2f02f
bhajanpreet <bhajanpreet25@gmail.com>
2020-04-07 12:15:12 -0400

    fixed contact us form now saving to db as well

--- routes/contactus.js
+++ routes/contactus.js
@@ -1,10 +1,36 @@
 var express = require("express");
 var router = express.Router();
+var { connect } = require("../database/connection");
 
 router.get("/", (req, res) => {
   res.render("contactus", {
     title: "We want to hear from you!",
     navcontactus: true,
+  });
+});
+
+router.post("/", (req, res) => {
+  connect((client, db) => {
+    let collection = db.collection("feedback");
+    collection
+      .insertOne(req.body)
+      .then((value) => {
+        client.close();
+        res.render("contactus", {
+          title: "We want to hear from you!",
+          navcontactus: true,
+          successmessage: "Your message delivered successfully",
+        });
+      })
+      .catch((error) => {
+        client.close();
+        res.render("contactus", {
+          title: "We want to hear from you!",
+          navcontactus: true,
+          failuremessage:
+            "Some Error Occurred while posting your message. Please retry.",
+        });
+      });
   });
 });
 
